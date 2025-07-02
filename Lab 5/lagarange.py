fea79e9c87d5e849387ed0f616631f79a35decfc
kaushalrijal <kaushalrijal091@gmail.com>
2025-07-02 14:47:56 +0545

    add visualization

--- Lab 5/lagarange.py
+++ Lab 5/lagarange.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import sympy as sp
+import matplotlib.pyplot as plt
 
 n = int(input("Enter the no. of data: "))
 
@@ -23,7 +24,27 @@ for i in range(n):
 
 poly = sp.simplify(poly)
 
-print("\nThe Lagarange polynomial is:\n", poly)
+print("The Lagarange polynomial is:\n", poly)
 
 int_val = poly.subs(x, xp)
 print(f"The interpolated value at x = {xp} is {int_val}")
+
+f = sp.lambdify(x, poly, "numpy")
+x_val = np.linspace(min(X)-2, max(X)+2, 1000)
+
+#visualize the output
+plt.figure()
+plt.plot(x_val, f(x_val), color="r", label=poly)
+plt.axvline(0, 0, color="black")
+plt.axhline(0, 0, color="black")
+plt.xlabel("x")
+plt.ylabel("f(x)")
+plt.legend()
+plt.grid(True)
+plt.title(f"Lagarange Interpolation")
+plt.scatter(X, f(X), color="blue")
+for i, val in enumerate(X):
+    plt.text(val, f(val), f'{val}')
+plt.scatter(xp, f(xp), color="red")
+plt.text(xp, f(xp), f'{xp}')
+plt.show()
